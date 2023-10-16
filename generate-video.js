ef42e062f7f3316067d2bfe594dfa7ec7f89ddf4
Liana NS <nlstmn@gmail.com>
2023-10-16 17:42:54 +0300

    Getting subtitles from the input video

---
+++ generate-video.js
@@ -0,0 +1,43 @@
+import fs from "fs-extra";
+import FFmpeg from "ffmpeg";
+
+const analyzeSubtitle = async function (subtitlePath) {
+    const transcript = fs.readFileSync(subtitlePath, "utf-8");
+    const cues = transcript
+        .split("\n\n")
+        .filter((line) => !line.startsWith("WEBVTT"))
+        .map((line) => {
+            const [time, text] = line.split("\n").slice(1);
+            const [start, end] = time.split(" --> ").map((t) => {
+                const [hms, ms] = t.split(".");
+                const [h, m, s] = hms.split(":");
+                return (
+                    parseInt(h) * 3600 +
+                    parseInt(m) * 60 +
+                    parseInt(s) +
+                    parseInt(ms) / 1000
+                );
+            });
+            return { start, end, text };
+        });
+};
+
+export const generateVideo = async function (videoPath, subtitlePath, resultPath) {
+    const video = await new FFmpeg(videoPath);
+    console.log(video)
+    video.addCommand(
+        "-vf",
+        `subtitles='${subtitlePath}':force_style='FontName=Arial,FontSize=24,PrimaryColour=&H0000FF&,BackColour=&HFFFF00&,Bold=1'`
+    );
+    // video.addCommand(
+    //     "-vf",
+    //     `drawbox=y=ih-50:color=yellow@0.5:width=iw:height=50:t=max`
+    // );
+    video.save(resultPath, (error, file) => {
+        if (error) {
+            console.error(error);
+        } else {
+            console.log("Video saved to", file);
+        }
+    });
+};
